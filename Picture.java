d4f32bcdc79ebe9d69a93d8272e698a2c5316211
mjond <mjd85@calvin.edu>
2016-09-24 14:47:21 -0400

    lab03 complete

--- Picture.java
+++ Picture.java
@@ -13,7 +13,7 @@ public class Picture extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
+        //co 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
         //ImageView.setVisibilty(View.INVISIBLE);
